fac0fbb2ffcb525890794074d20efe4ba7ef9ad1
Andy Osei <anyosei36@gmail.com>
2023-05-11 12:06:13 +0000

    change logo

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Otter from "./otter.png";
+import ExplorerLogo from "./logo_block_explorer.webp";
 
 const Logo: React.FC = () => (
   <div className="text-6xl text-link-blue font-title font-bold cursor-default flex items-center justify-center space-x-4">
     <img
       className="rounded-full"
-      src={Otter}
+      src={ExplorerLogo}
       width={96}
       height={96}
       alt="An otter scanning"
