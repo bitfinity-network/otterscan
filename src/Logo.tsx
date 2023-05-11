28863a4974ef92bad8268ad4ee993233f3b83fb5
Andy Osei <anyosei36@gmail.com>
2023-05-11 12:24:30 +0000

    update title

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import ExplorerLogo from "./logo_block_explorer.webp";
 
 const Logo: React.FC = () => (
-  <div className="text-6xl text-link-blue font-title font-bold cursor-default flex items-center justify-center space-x-4">
+  <div className="text-4xl text-link-blue font-title font-bold cursor-default flex items-center justify-center space-x-4">
     <img
-      className="rounded-full"
       src={ExplorerLogo}
       width={96}
       height={96}
       alt="An otter scanning"
       title="An otter scanning"
     />
-    <span>Otterscan</span>
+    <span>Bitfinity Block Explorer</span>
   </div>
 );
 
