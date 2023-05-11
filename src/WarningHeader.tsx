4295217c48b2d450593ae747f09bb64e0e406de1
Andy Osei <anyosei36@gmail.com>
2023-05-11 12:40:58 +0000

    add testnet indicator

--- src/WarningHeader.tsx
+++ src/WarningHeader.tsx
@@ -22,7 +22,13 @@ const WarningHeader: React.FC = () => {
   }
   return (
     <div className="w-full bg-orange-400 text-white text-center font-bold px-2 py-1">
-      You are on {chainMsg}
+      <div className="absolute left-8 flex flex-row items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6">
+          <circle cx="3" cy="3" r="3" fill="#80d6a1"></circle>
+        </svg>
+        <span className="ml-2">Testnet</span>
+      </div>
+      <div>You are on {chainMsg}</div>
     </div>
   );
 };
